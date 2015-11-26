7c41492ea3f1801589112fe4bf813094627d52e5
chimanaco <chimanaco@gmail.com>
2015-11-26 20:47:37 +0900

    Fixed error on the example on v0.9.0

--- ofxPSBlendExample/src/main.cpp
+++ ofxPSBlendExample/src/main.cpp
@@ -4,9 +4,10 @@
 //========================================================================
 int main( ){
 
-    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);  
-	ofSetupOpenGL(1024,768, OF_WINDOW);			// <-------- setup the GL context
-
+    ofGLWindowSettings s;
+    s.setGLVersion(3,2);
+    ofCreateWindow(s);
+    
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
